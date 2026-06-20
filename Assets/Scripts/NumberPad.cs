5cc759b567469ab7b17a23e648098b20d76e2282
Md Tahmid Wasif <mxw9311@mavs.uta.edu>
2026-06-20 00:57:18 -0500

    Number pad: Done

--- Assets/Scripts/NumberPad.cs
+++ Assets/Scripts/NumberPad.cs
@@ -1,6 +1,58 @@
+using System.Threading;
+using TMPro;
 using UnityEngine;
 
 public class NumberPad : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI codeText;
+    [SerializeField] private GameObject keycard;
     
+    private static string PASSWORD = "7639";
+    private static int NUM_KEYS = 4;
+    
+    private static TextMeshProUGUI _codeText;
+    private static GameObject _keycard;
+    private static int _numOfKeyPressed;
+    private static string _enteredCode;
+    private static bool _isCorrect;
+
+    void Start()
+    {
+        _codeText = codeText;
+        _keycard = keycard;
+        keycard.SetActive(false);
+        _numOfKeyPressed = 0;
+        _enteredCode = "";
+    }
+
+    public static void KeyPressed(string code)
+    {
+        if (_isCorrect) return;
+        
+        _codeText.text += "*";
+        _enteredCode += code;
+        _numOfKeyPressed++;
+        
+        Debug.Log("# of digits: " + _numOfKeyPressed);
+        Debug.Log("Entered Code: " + _enteredCode);
+
+        if (_numOfKeyPressed < NUM_KEYS) return;
+        
+        Debug.Log("PASSED INITIAL INSPECTION");
+        if (!_enteredCode.Equals(PASSWORD))
+        {
+            Debug.Log("INCORRECT");
+            _codeText.text = "INCORRECT";
+            _enteredCode = "";
+            _numOfKeyPressed = 0;
+            _codeText.text = "";
+        }
+        else
+        {
+            Debug.Log("CORRECT");
+            _isCorrect = true;
+            _codeText.text = "CORRECT";
+            _keycard.SetActive(true);
+        }
+    } 
 }
--- Assets/Scripts/TouchButton.cs
+++ Assets/Scripts/TouchButton.cs
@@ -1,18 +1,16 @@
 using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class TouchButton : XRBaseInteractable
 {
-    private int buttonNumber;
+    private string buttonNumber;
     private Color originalColor;
 
     void Start()
     {
-        char charNum = gameObject.name.Last();
-        buttonNumber = int.Parse(charNum.ToString());
+        buttonNumber = gameObject.name.Last().ToString();
         originalColor = GetComponent<MeshRenderer>().material.color;
     }
     protected override void OnHoverEntered(HoverEnterEventArgs args)
@@ -20,9 +18,11 @@ public class TouchButton : XRBaseInteractable
         base.OnHoverEntered(args);
 
         Debug.Log("Hover Entered");
-        
         Debug.Log("Button #: " + buttonNumber);
+        
         GetComponent<MeshRenderer>().material.color = Color.green;
+        
+        NumberPad.KeyPressed(buttonNumber);
     }
 
     protected override void OnHoverExited(HoverExitEventArgs args)
